13cf404e6d48af618cb8d5d2dd135001aa8f4d69
Brittani S Avery <brittani.s.avery@gmail.com>
2023-11-02 19:29:49 -0500

    Add footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Darker Grotesque Variable",
+          "Darker Grotesque",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        serif: ["Cormorant SC", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
